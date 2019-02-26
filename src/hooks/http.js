9cbd76a9350fc8a64cd9385a4b3121a930d6bb53
vinniiotchkov <vinniiotchkov@gmail.com>
2019-02-26 00:02:54 -0700

    Completed project

---
+++ src/hooks/http.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+export const useHttp = (url, dependencies) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(url)
+      .then(response => {
+        if (!response.ok) throw new Error('Failed to fetch.');
+        return response.json();
+      })
+      .then(data => {
+        setIsLoading(false);
+        setData(data);
+      })
+      .catch(err => console.log(err));
+  }, dependencies);
+  return [isLoading, data];
+};
+
+export default useHttp;
